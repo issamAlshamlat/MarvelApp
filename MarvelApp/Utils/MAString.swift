46e61832988b101446a0b42f35f73ec9b72d231d
@issamAlshamlat42 <issamalshamlat42@gmail.com>
2022-09-16 23:32:58 +0300

    Add Splash Image

--- MarvelApp/Utils/MAString.swift
+++ MarvelApp/Utils/MAString.swift
@@ -17,4 +17,13 @@ extension String {
     //MARK: - CharacterListViewController
     static let welcome_to_marvel_title_label = "Welcome To Marvel"
     
+    //MARK: - CharacterDetailsViewController
+    static let comics_label = "Comics"
+    static let events_label = "Events"
+    static let series_label = "Series"
+    static let stories_label = "Stories"
+    
+    //MARK: - CharacterStoryCollectionViewCell
+    static let creators_label = "Creators"
+    static let characters_label = "Characters"
 }
